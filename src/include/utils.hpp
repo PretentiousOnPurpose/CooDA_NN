167249c0b6ffaaab39a8c674ac65ac6000614411
Yashwanth Ramesh Kumar <yash.rk@outlook.com>
2023-12-10 09:52:51 +0100

    Some function declarations added

--- src/include/utils.hpp
+++ src/include/utils.hpp
@@ -1,4 +1,11 @@
 #include <iostream>
 
 
-float * matMul(float * x, float * y);
+float ** matMul(float ** x, float ** y, char * device);
+float ** strassenMatMul(float ** x, float ** y, char * device);
+float * FFT(float * x, int N, char * device);
+float * primeFFT(float * x, int N, char * device);
+float * conv1D(float * x, float * y, char * device);
+float * conv2D(float * x, float * y, char * device);
+float * xcorr1D(float * x, float * y, char * device);
+float * xcorr2D(float * x, float * y, char * device);
